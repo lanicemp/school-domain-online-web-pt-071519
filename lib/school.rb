8db093f375ea73519ec9f3deae8be9636032bc6c
LaNice Powell <lanicemp@aol.com>
2019-08-05 15:02:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,10 +13,10 @@ class School
     @roster
   end 
   
-  def add_student(student_name, grade)
+  def add_student(name, grade)
     
      
-     # binding.pry 
+      binding.pry 
    # end 
     #roster[grade] = []
     #roster[grade] << name 
