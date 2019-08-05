a9cdb52712fdef68ffb415a0039b610d86a73cc8
LaNice Powell <lanicemp@aol.com>
2019-08-05 14:24:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :student_name, :grade
+  attr_reader :roster
+  
+  def initialize(roster)
+    @name = name 
+    @roster = {} 
+  end 
+  
+  def roster 
+    @roster
+  end 
+  
+  def add_student(student_name, grade)
+    
+     
+      binding.pry 
+    end 
+    #roster[grade] = []
+    #roster[grade] << name 
+  end 
+  
+  #def add_student
+   # @student
+  #end
+ # roster << add_student
+  
+end 
